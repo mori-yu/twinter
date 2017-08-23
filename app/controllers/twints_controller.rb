84a081ad1cb6e79de6f20008e74468b0f32cc74b
Yu Morishige <mori-yu@nttpc.co.jp>
2017-08-23 02:33:22 +0000

    完成かな

--- app/controllers/twints_controller.rb
+++ app/controllers/twints_controller.rb
@@ -24,6 +24,27 @@ class TwintsController < ApplicationController
         @twint = Twint.find(params[:id])
     end
     
+    def update
+        @twint = Twint.find(params[:id])
+        @twint2 = Twint.new(twints_params)
+        if @twint2.invalid?
+            @twint2.errors.full_messages.each do |msg|
+                #これたぶんためだ・・・
+                #複数エラーがあったときの挙動・・・
+                redirect_to twints_path, danger: msg
+            end
+        else 
+            @twint.update(twints_params)
+            redirect_to twints_path, success: "ブログを編集しました！"
+        end
+    end
+    
+    def destroy
+        @twint = Twint.find(params[:id])
+        @twint.destroy
+        redirect_to twints_path, success: "ブログを削除しました..."
+    end
+    
     private
     def twints_params
       params.require(:twint).permit(:content)
